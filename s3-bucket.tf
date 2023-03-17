82c1da6497203fc6f390c2f46cc881fa817c0b9a
xxobby <xxompletehj@gmail.com>
2023-03-17 14:30:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/instruqt-201/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "hj-aws"
-
-  versioning = {
-    enabled = true
-  }
-
 }
